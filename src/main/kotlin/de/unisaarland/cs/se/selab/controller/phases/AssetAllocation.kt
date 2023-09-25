138720d3a40d02d5535dc077765496315a41e268
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-25 16:52:36 +0200

    AssetAllocation add

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/AssetAllocation.kt
@@ -0,0 +1,28 @@
+package de.unisaarland.cs.se.selab.controller.phases
+
+import de.unisaarland.cs.se.selab.model.Model
+
+class AssetAllocation {
+
+    /**
+     * Allocate and reallocate assets for each emergency.
+     * */
+    fun execute(m: Model): Unit {
+        // todo
+    }
+
+    /**
+     * Sort Emergencies firstly by severity and then by id.
+     * */
+    private fun sortEmergencies(emergencies: MutableList<Emergency>): Unit {
+        // todo
+    }
+
+    /**
+     * Sort Vehicles by id.
+     * */
+    private fun sortVehicles(vehicles: List<Vehicle>): Unit {
+        // todo
+    }
+
+}
