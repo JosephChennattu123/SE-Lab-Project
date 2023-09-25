c9c8a4bee5a210e7472bdcee7f3c8aaf36f01a0a
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-25 18:05:12 +0200

    added EmergencyValidator

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/config/EmergencyValidator.kt
@@ -0,0 +1,60 @@
+package de.unisaarland.cs.se.selab.config
+
+import de.unisaarland.cs.se.selab.model.Emergency
+
+/**
+ * Validates emergencies
+ */
+class EmergencyValidator : BasicValidator() {
+
+    /**
+     * Validates the information for emergencies and creates emergencies.
+     *
+     * @return the list of emergencies created
+     */
+    fun validate(): List<Emergency> {
+        // TODO
+        return listOf()
+    }
+
+    /**
+     * Checks that the corresponding village (or county) exists
+     *
+     * @return true if villages are valid
+     */
+    private fun validateVillagesExist(): Boolean {
+        // TODO
+        return false
+    }
+
+    /**
+     * Check that the corresponding road exists in the village or county
+     *
+     * @return true if the roads exist
+     */
+    private fun validateRoadExistsInVillageOrCounty(): Boolean {
+        // TODO
+        return false
+    }
+
+    /**
+     * Check that the severity is in bounds
+     *
+     * @return true if severities are valid
+     */
+    private fun validateSeverityBounds(): Boolean {
+        // TODO
+        return false
+    }
+
+    /**
+     * Check that the maxDuration does not exceed the handle time
+     * Also checks that the duration and handle time are valid
+     *
+     * @return true if maxDurations and handleTimes are valid
+     */
+    private fun validateMaxDurationNotExceedHandleTime(): Boolean {
+        // TODO
+        return false
+    }
+}
