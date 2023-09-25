9829c90aeef35a737cae8349a4bc208d28bbd29b
Ferdinand <blockletsfun@gmail.com>
2023-09-25 16:53:20 +0200

    Added ControlCenter (formally EMCC)

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
@@ -0,0 +1,14 @@
+package de.unisaarland.cs.se.selab.controller
+
+class ControlCenter {
+    /***
+     * Runs the simulation until termination */
+    fun simulate(): Boolean {
+        return false
+    }
+
+    /***
+     * Progresses the simulation by 1 tick */
+    fun tick() {
+    }
+}
