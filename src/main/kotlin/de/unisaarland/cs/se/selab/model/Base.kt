811bdf06bb138e8fffe8432dddd581caf9ba0f83
JosephChennattu123 <josephchennattu4@gmail.com>
2023-09-25 21:17:25 +0200

    added Base and enum BaseType is inside the class itself

---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/Base.kt
@@ -0,0 +1,15 @@
+package de.unisaarland.cs.se.selab.model
+
+class Base {
+    fun addEmergency(emId: Int): Unit {
+        //TODO
+    }
+
+    fun addVehicle(vehicleId: Int): Unit {
+        //TODO
+    }
+}
+
+enum class BaseType {
+    FIRE_STATION, POLICE_STATION, HOSPITAL
+}
