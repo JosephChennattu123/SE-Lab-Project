7cebb784768ffc81f6ab1f531ef95f36f73eacbd
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-25 22:32:39 +0200

    removed method "Vertex" from class vertex (should be a constructor, used "default" one)

--- src/main/kotlin/de/unisaarland/cs/se/selab/model/Vertex.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/Vertex.kt
@@ -2,10 +2,7 @@ package de.unisaarland.cs.se.selab.model
 
 import java.util.*
 
-class Vertex {
-    fun Vertex(vertexId: Int) {
-        //TODO
-    }
+class Vertex(val vertexId: Int) {
 
     fun getEdges(reverse: Boolean): PriorityQueue<Edge> {
         //TODO
