5d9a1bf9b9f10d052c9e8cbcc35c95b2a3c08485
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-26 12:30:13 +0200

    removed unneeded imports in EmergencyDistribution

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
@@ -1,7 +1,5 @@
 package de.unisaarland.cs.se.selab.controller.phases
 
-import de.unisaarland.cs.se.selab.model.Base
-import de.unisaarland.cs.se.selab.model.Emergency
 import de.unisaarland.cs.se.selab.model.Model
 
 class EmergencyDistribution {
