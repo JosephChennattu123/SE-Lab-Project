74ff2a470f085052ef7697607825eb57c3f36c09
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-26 17:26:36 +0200

    added more fields to Connection

--- src/main/kotlin/de/unisaarland/cs/se/selab/config/Connection.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/config/Connection.kt
@@ -1,13 +1,18 @@
 package de.unisaarland.cs.se.selab.config
 
+import de.unisaarland.cs.se.selab.model.map.PrimaryType
+import de.unisaarland.cs.se.selab.model.map.SecondaryType
+
 /**
  * @param vertexId the target of the connection
  * @param name the name of the connection(road)
  * @param weight the weight of the connection
  * @param direction the direction of the connection
  */
-data class Connection(val vertexId: Int, val name: String,
-                      val weight: Int, val direction: Direction)
+data class Connection(
+    val vertexId: Int, val name: String, val weight: Int, val height: Int, val direction: Direction,
+    val primary: PrimaryType, val secondary: SecondaryType, val villageName: String, val roadName: String
+)
 
 /**
  * direction of a road / connection:
@@ -17,6 +22,7 @@ enum class Direction {
      * only from source to target of road / connection
      */
     ONEWAY,
+
     /**
      * both direction
      */
