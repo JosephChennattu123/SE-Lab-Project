962f28501ef8bad266480d625ce07a23506cba7f
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-26 10:13:50 +0200

    add phases

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/UpdatePhase.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/UpdatePhase.kt
@@ -1,9 +1,42 @@
 package de.unisaarland.cs.se.selab.controller.phases
 
+import de.unisaarland.cs.se.selab.model.Emergency
 import de.unisaarland.cs.se.selab.model.Model
+
+import de.unisaarland.cs.se.selab.model.Vehicle
+
 
 class UpdatePhase {
     fun execute(m: Model): Unit {
         // todo
+    }
+
+    private fun processVehicles(vehicles: List<Vehicle>, emergencies: List<Emergency>): Unit {
+        // todo
+    }
+
+    private fun processEmergencies(model: Model): Unit {
+        // todo
+    }
+
+    private fun processActiveEvents(): Unit {
+        // todo
+    }
+
+    private fun processPostponedEvents(): Unit {
+        // todo
+    }
+
+    private fun timeUpdate(model: Model): Unit {
+        // todo
+    }
+
+    private fun printLog(vehicles: List<Vehicle>): Unit {
+        // todo
+    }
+
+    private fun collectArrivedV(vehicles: List<Vehicle>): List<Vehicle> {
+        // todo
+        return TODO()
     }
 }
