1526af9f240d9f16acf08ad6c419d6e74192787b
Bashar Hamade <baha00002@stud.uni-saarland.de>
2023-09-26 10:11:42 +0100

    Logger and EmergencyDistribution added and modified

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
@@ -6,7 +6,7 @@ import de.unisaarland.cs.se.selab.model.Model
 
 class EmergencyDistribution {
 
-    fun execute(m: Model) {
+    public fun execute(m:Model) {
         getEmergenciesFor(0)
         findNearestBase()
         distributeEmergencies()
@@ -15,19 +15,40 @@ class EmergencyDistribution {
     }
 
     //List<Emergency>
-    private fun getEmergenciesFor(tick: Int): List<Emergency> {
-        return listOf()
-    }
+    /**
+     * @param tick : gets current tick
+     * @return list of emergencies at the current tick
+     * Get the emergecies that are happening at the current tick
+     */
+    private fun getEmergenciesFor(tick:Int) : List<Emergency> {
 
-    private fun findNearestBase(e: Emergency): Base {
-        return Base()
-    }
-
-    private fun distributeEmergencies(l: List<Emergency>) {
 
     }
+    /**
+     * @param e : the emergency to be handled which needs a near base to be assigned to
+     * @return the nearest base found
+     * Get the nearest base to an emergency
+     */
 
-    private fun getSeverity(emergency: Emergency) {
+    private fun findNearestBase(e: Emergency):Base {
+
+    }
+    /**
+     * @param l : list of the emergencies to be distributed
+     * @return the nearest base found
+     * Get the nearest base to an emergency
+     */
+
+    private fun distributeEmergencies(l:List<Emergency>) {
+
+    }
+
+    /**
+     * @param emergency : the emergency to get the severity for
+     * @return the severity of the severity
+     * Takes an emergency and returns its severity
+     */
+    private fun getSeverity(emergency:Emergency) {
 
     }
 
---
+++ src/main/kotlin/de/unisaarland/cs/se/selab/util/Logger.kt
@@ -0,0 +1,86 @@
+package de.unisaarland.cs.se.selab.util
+
+class Logger {
+
+
+    public fun logParsingValidationSuccess(filename:String) {
+
+    }
+    public fun logFileInvalid(filename: String){
+
+    }
+    public fun logTick(tick:Int){
+
+    }
+    public fun logEmergencyAssigned(id1:Int,id2:Int)
+    {
+
+    }
+    public fun logAssetAllocated(assetId:Int,emergencyId:Int,arrivesInt: Int)
+    {
+
+    }
+    public fun logAssetReallocated(assetId:Int,emergencyId:Int)
+    {
+
+    }
+    public fun logRequest(requestId:Int,targetBaseId:Int,emergencyId: Int)
+    {
+
+    }
+    public fun logRequestFailed(emergencyId: Int)
+    {
+
+    }
+    public fun logAssetArrived(assetId: Int,vertexId:Int)
+    {
+
+    }
+    public fun logEmergencyHandlingStart(emergencyId: Int)
+    {
+
+    }
+    public fun logEmergencyResolve(emergencyId: Int)
+    {
+
+    }
+    public fun logEmergencyFailed(emergencyId: Int)
+    {
+
+    }
+
+    public fun logEventEnded(eventId:Int)
+    {
+
+    }
+    public fun logEventTriggered(eventId:Int)
+    {
+
+    }
+    public fun logAssetRerouted(assetsAmount: Int){
+
+    }
+    public fun logSimulationEnded()
+    {
+
+    }
+    public fun logNumberOfReroutedAssets(reroutedAssetsAmount : Int){
+
+    }
+    public fun logNumberOfRecievedEmergencies(recievedEmergenciesAmount : Int){
+
+    }
+    public fun logNumberOfOngoingEmergencies(ongoingEmergenciesAmount : Int){
+
+    }
+    public fun logNumberOfFailedEmergencies(failedEmergenciesAmount : Int){
+
+    }
+    public fun logNumberOfResolvedEmergencies(resolvedEmergenciesAmount : Int){
+
+    }
+
+
+
+
+}
