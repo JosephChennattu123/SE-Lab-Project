c9b3c4a275bed65de673e6c73eeaced76e340ae0
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-26 17:25:26 +0200

    implemented RoadProperties incl. documentation of constructor parameters

--- src/main/kotlin/de/unisaarland/cs/se/selab/model/map/RoadProperties.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/map/RoadProperties.kt
@@ -1,4 +1,12 @@
 package de.unisaarland.cs.se.selab.model.map
 
-class RoadProperties {
+
+/**
+ * @param roadType the primary type of the road
+ * @param secondaryType the secondary type of the road
+ * @param weight the weight of the edge (the road)
+ * @param height the height of the road
+ */
+class RoadProperties(val roadType: PrimaryType, val secondaryType: SecondaryType, val weight: Int, val height: Int) {
+    var factor: Int = 1
 }
