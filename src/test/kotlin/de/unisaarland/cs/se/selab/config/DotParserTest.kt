1597825265b606138bbae3402c91d46cd5210510
DanielPC <dash00001@stud.uni-saarland.de>
2023-09-26 19:31:49 +0200

    able to parse multiple edges

--- src/test/kotlin/de/unisaarland/cs/se/selab/config/DotParserTest.kt
+++ src/test/kotlin/de/unisaarland/cs/se/selab/config/DotParserTest.kt
@@ -7,31 +7,32 @@ import java.io.File
 class DotParserTest {
     @Test
     fun parseCountyName() {
-        val newLineBeforeCurly : String = "digraph"+" " + "t3st " + "\n" + " {"
+        val newLineBeforeCurly: String = "digraph" + " " + "t3st " + "\n" + " {"
         val onlySpaces = "digraph t3st {"
-        val onlyNewLine = "\n" + "digraph" + "\n" +  "t3st" + "\n" + "{"
-        val list : List<String> = listOf(newLineBeforeCurly, onlySpaces, onlyNewLine)
+        val onlyNewLine = "\n" + "digraph" + "\n" + "t3st" + "\n" + "{"
+        val list: List<String> = listOf(newLineBeforeCurly, onlySpaces, onlyNewLine)
     }
 
     @Test
     fun parseVertices() {
-       val input = "{" + "\n"+ "1;" + "2" + "\n" + ";" + "32 ;" + "\n" + "7 ->" + "}"
+        val input = "{" + "\n" + "1;" + "2" + "\n" + ";" + "32 ;" + "\n" + "7 ->" + "}"
     }
 
     @Test
     fun parseEdges() {
-        //val input = ";" + "1" + "\n" + "->" + "\n" + "2" + "\n" + "[label=" + "\n" + "test;" + "\n" + "labelb=text;" + "];"
-        val input = """5 -> 6 [village=Saarland; name=Motorway; heightLimit=10; weight = 16; primaryType=countyRoad; secondaryType=none;];
+        // val input = ";" + "1" + "\n" + "->" + "\n" + "2" + "\n" + "[label=" + "\n" + "test;" + "\n" + "labelb=text;" + "];"
+        val input = """5 -> 6 [village=Saarland; name=Motorway; heightLimit=a; weight = 16; primaryType=countyRoad; secondaryType=none;];
+            
     10 -> 11 [village=Saarland; name=Countryroad; heightLimit=2; weight = 39; primaryType=countyRoad; secondaryType=tunnel;];
+    
     15 -> 1 [village=Saarland; name=Fastlane; heightLimit=10; weight = 29; primaryType=countyRoad; secondaryType=none;];"""
-        //val regex ="""\s*[0-9]+\s*->\s*[0-9]+\s*\[([a-z]*\s*=\s*([a-z0-9]|_|\.)*\s*;\s*)*]""".toRegex()
-        val regex = """\s*[0-9]+\s*->\s*[0-9]\s*\[(\s*[a-z]+\s*=\s*[a-zA-z0-9]+|_|\.\s*]\s*;*\s)*]\s*;\s*""".toRegex()
+
+        // val regex ="""\s*[0-9]+\s*->\s*[0-9]+\s*\[([a-z]*\s*=\s*([a-z0-9]|_|\.)*\s*;\s*)*]""".toRegex()
+        val fileContent = File("D:\\Projects\\SE Lab 2023\\src\\systemtest\\resources\\mapFiles\\example_map.dot").readText()
+        val regex = """\s*[0-9]+\s*->\s*[0-9]+\s*\[(\s*[a-zA-z]+\s*=\s*[a-zA-z0-9]+\s*;\s*)*\s*]\s*;""".toRegex()
         val matchResult = regex.findAll(input)
-        val matchedString = matchResult.map{ it.groupValues[0].replace("\n","")}
+        val matchedString = matchResult.map { it.groupValues[0].replace("\\s".toRegex(),"") }
             .toList()
         println("edges:$matchedString")
-
-
     }
-
 }
