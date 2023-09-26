5d969551d7aee74438210f027df4ed0467eba274
Ferdinand <blockletsfun@gmail.com>
2023-09-26 10:43:36 +0200

    Clean-up and added documentation

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
@@ -6,7 +6,7 @@ import de.unisaarland.cs.se.selab.model.Model
 
 class EmergencyDistribution {
 
-    public fun execute(m:Model) {
+    public fun execute(m: Model) {
         getEmergenciesFor(0)
         findNearestBase()
         distributeEmergencies()
@@ -20,26 +20,28 @@ class EmergencyDistribution {
      * @return list of emergencies at the current tick
      * Get the emergecies that are happening at the current tick
      */
-    private fun getEmergenciesFor(tick:Int) : List<Emergency> {
+    private fun getEmergenciesFor(tick: Int): List<Emergency> {
 
 
     }
+
     /**
      * @param e : the emergency to be handled which needs a near base to be assigned to
      * @return the nearest base found
      * Get the nearest base to an emergency
      */
 
-    private fun findNearestBase(e: Emergency):Base {
+    private fun findNearestBase(e: Emergency): Base {
 
     }
+
     /**
      * @param l : list of the emergencies to be distributed
      * @return the nearest base found
      * Get the nearest base to an emergency
      */
 
-    private fun distributeEmergencies(l:List<Emergency>) {
+    private fun distributeEmergencies(l: List<Emergency>) {
 
     }
 
@@ -48,7 +50,7 @@ class EmergencyDistribution {
      * @return the severity of the severity
      * Takes an emergency and returns its severity
      */
-    private fun getSeverity(emergency:Emergency) {
+    private fun getSeverity(emergency: Emergency) {
 
     }
 
--- src/main/kotlin/de/unisaarland/cs/se/selab/model/EmergencyRequirement.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/EmergencyRequirement.kt
@@ -1,5 +1,6 @@
 package de.unisaarland.cs.se.selab.model
-
+/**
+ * Type and number of asset required for the fulfillment of an Emergency */
 class EmergencyRequirement {
-
+    // TODO
 }
