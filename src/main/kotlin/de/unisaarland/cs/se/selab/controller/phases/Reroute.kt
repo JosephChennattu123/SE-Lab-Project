684e3d41c0ae95e9443dcdaea80189b7e686a3c9
Ferdinand <fesc00005@stud.uni-saarland.de>
2023-09-26 15:08:03 +0200

    Added functionality and documentation

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/Reroute.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/Reroute.kt
@@ -1,9 +1,20 @@
 package de.unisaarland.cs.se.selab.controller.phases
 
 import de.unisaarland.cs.se.selab.model.Model
+import de.unisaarland.cs.se.selab.model.Vehicle
+import de.unisaarland.cs.se.selab.model.VehicleStatus
+import de.unisaarland.cs.se.selab.util.Dijkstra
 
 class Reroute {
+    /**
+     * Iterates through all vehicles in the model and calculates new paths for all currently moving vehicles
+     * @param model The model
+     * */
     fun execute(model: Model) {
-
+        for (v : Vehicle in model.getSortedVehicleList()) {
+            if (v.status == VehicleStatus.TO_EMERGENCY || v.status == VehicleStatus.RETURNING) {
+                v.positionTracker.path = Dijkstra.getShortestPathFromVertexToEdge()
+            }
+        }
     }
 }
