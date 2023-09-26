bbc5ddc3db6bc2d693c58f127ceb7312d79df287
Bashar Hamade <baha00002@stud.uni-saarland.de>
2023-09-26 16:28:06 +0100

    update logger

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
@@ -1,45 +1,49 @@
+import de.unisaarland.cs.se.selab.model.BaseType
 import de.unisaarland.cs.se.selab.model.Emergency
+import de.unisaarland.cs.se.selab.model.EmergencyType
 import de.unisaarland.cs.se.selab.model.Model
+import de.unisaarland.cs.se.selab.util.Dijkstra
 import de.unisaarland.cs.se.selab.util.Logger
 
 class EmergencyDistribution {
 
 
-    public fun execute(m: Model) {
+    public fun execute(model: Model) {
         //get emergencies for current tick
-        var currentEmergencies = m.tickToEmergencyId.get(0)
+        var currentEmergencies = model.tickToEmergencyId.get(0)
 
         //get graph from model
-        var graph = m.graph
-        var loc = e.location
+        var graph = model.graph
+
 
         for (e in currentEmergencies)
         {
             //iterate over emergencies and get nearest base using one of the dijkstra methods
             var nearestbaseId : Int = 0
+            var loc = e.location
 
             //This seems very wrong,but I need to provide a basetype for getnearestbasetoEdge,so I will have multiple if statements for this purpose
             if(e.type = EmergencyType.CRIME)
             {
-                nearestbaseId = Dijkystra.getnearestbasetoEdge(graph,loc,BaseType.POLICE_STATION)
+                nearestbaseId = Dijkstra.getnearestbasetoEdge(graph,loc, BaseType.POLICE_STATION)
             }
             if(e.type = EmergencyType.FIRE)
             {
-                nearestbaseId = Dijkystra.getnearestbasetoEdge(graph,loc,BaseType.FIRE_STATION)
+                nearestbaseId = Dijkstra.getnearestbasetoEdge(graph,loc,BaseType.FIRE_STATION)
             }
             if(e.type = EmergencyType.ACCIDENT || e.type = EmergencyType.MEDICAL)
             {
-                nearestbaseId =  Dijkystra.getnearestbasetoEdge(graph,loc,BaseType.HOSPITAL)
+                nearestbaseId =  Dijkstra.getnearestbasetoEdge(graph,loc,BaseType.HOSPITAL)
             }
             //assign the base Id to the emergency
             //e.baseId = nearestbaseId
 
             //assign the emergency Id to the base
-            m.getBasebyId(nearestbaseId).addEmergency(e)
+            model.getBasebyId(nearestbaseId).addEmergency(e)
 
 
             //add emergency to active emergencies in the model
-            m.addToAssignedEmergencies(nearestbaseId)
+            model.addToAssignedEmergencies(nearestbaseId)
 
             Logger.logEmergencyAssigned(e.id,nearestbaseId)
 
