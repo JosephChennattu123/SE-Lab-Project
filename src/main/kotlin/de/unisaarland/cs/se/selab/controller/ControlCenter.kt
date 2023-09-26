646057062d10b906879843b8fcd0400e968537fd
SimonKirchner <siki00001@stud.uni-saarland.de>
2023-09-26 17:42:58 +0200

    added "model: Model" as a property of ControlCenter, added TODO() in simulate, tick

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
@@ -1,14 +1,22 @@
 package de.unisaarland.cs.se.selab.controller
 
-class ControlCenter {
+import de.unisaarland.cs.se.selab.model.Model
+
+/**
+ * The ControlCenter runs the simulation.
+ *
+ * @param model contains the data to simulate
+ */
+class ControlCenter(val model: Model) {
     /***
      * Runs the simulation until termination */
     fun simulate(): Boolean {
-        return false
+        TODO()
     }
 
     /***
      * Progresses the simulation by 1 tick */
     fun tick() {
+        TODO()
     }
 }
