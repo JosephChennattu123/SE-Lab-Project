b64e093b2a7fb29c2843169406c3b55362aa3f2b
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-27 12:52:31 +0200

    phase

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/AssetAllocation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/AssetAllocation.kt
@@ -18,15 +18,15 @@ class AssetAllocation {
                 AssetManager.allocateAssetsToEmergency(model, emergency, vehicles)
                 if (!emergency.isFulfilled()) {
                     // reallocate
-                    val vehiclesCanReroute =
-                        getVehiclesCanReroute(emergency, mainBase, vehicles, model)
-                    AssetManager.allocateAssetsToEmergency(model, emergency, vehiclesCanReroute)
-                    // if allocate & reallocate not failed, then change this canRequest
-                    if (emergency.assignedVehicleIDs.isNotEmpty()) emergency.canRequest = true
-                    if (!emergency.isFulfilled() && emergency.canRequest) {
-                        // request
-
+                    val lowerSeverityEmergencies =
+                        model.getAssignedEmergenciesByIds(mainBase.assignedEmergencies)
+                            .filter { it.severity < emergency.severity }
+                    for (lowEmergency in lowerSeverityEmergencies) {
+                        val vehiclesCanReroute =
+                            model.getVehiclesByIds(lowEmergency.assignedVehicleIDs)
+                                .filter { it.baseID == mainBase.baseId }
                     }
+
                 }
             }
         }
@@ -47,30 +47,5 @@ class AssetAllocation {
      * */
     private fun sortAtBaseVehicles(vehicles: List<Vehicle>): List<Vehicle> {
         return vehicles.filter { it.status == VehicleStatus.AT_BASE }.sortedBy { it.vehicleID }
-    }
-
-    /**
-     * @return Vehicles can reroute:
-     * 1. on their way to an emergency with lower severity
-     * 2. or on their way back to the base
-     * */
-    private fun getVehiclesCanReroute(
-        emergency: Emergency,
-        mainBase: Base,
-        vehicles: List<Vehicle>,
-        model: Model
-    ): MutableList<Vehicle> {
-        val vehiclesCanReroute = mutableListOf<Vehicle>()
-        val lowerSeverityEmergencies =
-            model.getAssignedEmergenciesByIds(mainBase.assignedEmergencies)
-                .filter { it.severity < emergency.severity }
-        for (lowEmergency in lowerSeverityEmergencies) {
-            vehiclesCanReroute.addAll(
-                model.getVehiclesByIds(lowEmergency.assignedVehicleIDs)
-                    .filter { it.baseID == mainBase.baseId && it.status == VehicleStatus.TO_EMERGENCY }
-            )
-        }
-        vehiclesCanReroute.addAll(vehicles.filter { it.status == VehicleStatus.RETURNING })
-        return vehiclesCanReroute
     }
 }
