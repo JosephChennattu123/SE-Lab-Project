c146b4af85f27d938d551179feb243c5b5ada193
Daniel Sherbakov <dash00001@stud.uni-saarland.de>
2023-09-27 14:41:56 +0200

    added translation from string to eventType (fromString()).

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/events/Event.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/events/Event.kt
@@ -1,5 +1,8 @@
 package de.unisaarland.cs.se.selab.controller.events
 
+/**
+ * Event superclass.
+ * */
 abstract class Event(val id: Int, val eventType: EventType, val start: Int, val duration: Int) {
 
     var status: EventStatus = EventStatus.NOT_SCHEDULED
@@ -9,21 +12,58 @@ abstract class Event(val id: Int, val eventType: EventType, val start: Int, val 
      * */
     abstract fun applyEffect(): Unit
 
+    /**
+     * Decrements the timer of the event.
+     * */
     abstract fun decrementTimer(): Unit
 }
 
+/**
+ * represents the type of event
+ * */
 enum class EventType {
-    RUSH_HOUR,
-    TRAFFIC_JAM,
-    CONSTRUCTION_SITE,
-    ROAD_CLOSURE,
-    VEHICLE_UNAVAILABLE
+    RUSH_HOUR, TRAFFIC_JAM, CONSTRUCTION_SITE, ROAD_CLOSURE, VEHICLE_UNAVAILABLE;
 
+    companion object {
+        /**
+         * deserializes a string to an EventType
+         * @param value the string to be deserialized
+         * @return the deserialized EventType
+         * */
+        fun fromString(value: String): EventType? {
+            return when (value) {
+                "RUSH_HOUR" -> RUSH_HOUR
+                "TRAFFIC_JAM" -> TRAFFIC_JAM
+                "CONSTRUCTION_SITE" -> CONSTRUCTION_SITE
+                "ROAD_CLOSURE" -> ROAD_CLOSURE
+                "VEHICLE_UNAVAILABLE" -> VEHICLE_UNAVAILABLE
+                else -> null
+            }
+        }
+    }
 }
 
+/**
+ * The status of an event.
+ * */
 enum class EventStatus {
+    /**
+     * The event is not scheduled yet.
+     * */
     NOT_SCHEDULED,
+
+    /**
+     * The event is scheduled but not active yet.
+     * */
     SCHEDULED,
+
+    /**
+     * The event is active and the effect of it is applied.
+     * */
     ACTIVE,
+
+    /**
+     * The event is finished and the effect of it is not applied anymore.
+     * */
     FINISHED
 }
