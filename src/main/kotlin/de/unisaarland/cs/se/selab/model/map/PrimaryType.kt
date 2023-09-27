4968bd9bc9ee5a9cc0e0295f58be7ddd4c99f6a0
Daniel Sherbakov <dash00001@stud.uni-saarland.de>
2023-09-27 15:34:08 +0200

    added fromString function in PrimaryType

--- src/main/kotlin/de/unisaarland/cs/se/selab/model/map/PrimaryType.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/map/PrimaryType.kt
@@ -1,5 +1,22 @@
 package de.unisaarland.cs.se.selab.model.map
 
+/**
+ * The primary type of road.
+ * */
 enum class PrimaryType {
-    MAIN, SIDE, COUNTY
+    MAIN, SIDE, COUNTY;
+
+    companion object {
+        /**
+         * deserialize a string to a [PrimaryType].
+         * */
+        fun fromString(value: String): PrimaryType? {
+            return when (value) {
+                "MAIN" -> MAIN
+                "SIDE" -> SIDE
+                "COUNTY" -> COUNTY
+                else -> null
+            }
+        }
+    }
 }
