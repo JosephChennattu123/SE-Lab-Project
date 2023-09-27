a93c61e59a88540e6102738a088747f1bf53920d
DanielPC <dash00001@stud.uni-saarland.de>
2023-09-27 23:49:25 +0200

    added missing oneWay flags for edges and functionality

--- src/main/kotlin/de/unisaarland/cs/se/selab/model/Path.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/Path.kt
@@ -1,4 +1,18 @@
 package de.unisaarland.cs.se.selab.model
 
-class Path(val vertexPath: List<Int>, val edgeWeights: List<Int>, val totalTicksToArrive: Int) {
+/**
+ * class for representing a path a vehicle has to travel to its destination.
+ * */
+class Path(
+    val vertexPath: List<Int>,
+    val edgeWeights: List<Int>,
+    val isOneWay: List<Boolean>,
+    val totalTicksToArrive: Int
+){
+    /**
+     * calculates the total weight of the entire path.
+     * */
+    fun getTotalDistance(): Int{
+        return edgeWeights.sum()
+    }
 }
