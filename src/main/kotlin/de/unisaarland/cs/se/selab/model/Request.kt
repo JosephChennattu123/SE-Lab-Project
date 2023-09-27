694b21a0125585bf98f4f71cd3d641ce9db363ba
Bashar Hamade <baha00002@stud.uni-saarland.de>
2023-09-27 11:03:21 +0100

    Created Request with auto-incrementing

--- src/main/kotlin/de/unisaarland/cs/se/selab/model/Request.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/model/Request.kt
@@ -1,5 +1,25 @@
 package de.unisaarland.cs.se.selab.model
 
-class Request {
+class Request private constructor(
+    val requestId: Int,
+    val mainBaseId: Int,
+    val emergencyId: Int,
+    val targetBaseId: Int,
+    var processesBases: Set<Int>
+) {
+    companion object {
+        private var currentRequestId = 0
 
+        fun createNewRequest(
+            mainBaseId: Int,
+            emergencyId: Int,
+            targetBaseId: Int,
+            processesBases: Set<Int>
+        ): Request {
+            val newRequest = Request(currentRequestId, mainBaseId, emergencyId, targetBaseId, processesBases)
+            currentRequestId++
+            return newRequest
+        }
+    }
 }
+
