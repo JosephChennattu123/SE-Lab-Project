4d8b1961406fd68234b532a630e22a6dd6eee31f
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-27 21:00:47 +0200

    add properties for GatherStatistics in Model

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
@@ -7,14 +7,14 @@ class GatherStatistics {
     fun execute(model: Model) {
         Logger.logSimulationEnded()
         // the number of rerouted assets
-//        Logger.logNumberOfReroutedAssets(model.)
+        Logger.logNumberOfReroutedAssets(model.numReroutedAssets)
         // the number of received emergencies
         Logger.logNumberOfRecievedEmergencies(model.emergencies.values.size)
         // the number of still ongoing emergencies
         Logger.logNumberOfOngoingEmergencies(model.assignedEmergencies.size)
         // the number of failed emergencies
-//        Logger.logNumberOfFailedEmergencies()
-        // g the number of resolved emergencies
-//        Logger.logNumberOfResolvedEmergencies()
+        Logger.logNumberOfFailedEmergencies(model.numFailedEmergencies)
+        // the number of resolved emergencies
+        Logger.logNumberOfResolvedEmergencies(model.numResolvedEmergency)
     }
 }
