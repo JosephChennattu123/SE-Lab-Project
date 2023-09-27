2ba77961b1535af5a2889f0b300a27b7e162815f
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-27 18:51:45 +0200

    GatherStatistics not done, need more properties in model to do the statistics

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
@@ -1,9 +1,20 @@
 package de.unisaarland.cs.se.selab.controller.phases
 
 import de.unisaarland.cs.se.selab.model.Model
+import de.unisaarland.cs.se.selab.util.Logger
 
 class GatherStatistics {
-    fun execute(m: Model): Unit {
-        // todo
+    fun execute(model: Model) {
+        Logger.logSimulationEnded()
+        // the number of rerouted assets
+//        Logger.logNumberOfReroutedAssets(model.)
+        // the number of received emergencies
+        Logger.logNumberOfRecievedEmergencies(model.emergencies.values.size)
+        // the number of still ongoing emergencies
+        Logger.logNumberOfOngoingEmergencies(model.assignedEmergencies.size)
+        // the number of failed emergencies
+//        Logger.logNumberOfFailedEmergencies()
+        // g the number of resolved emergencies
+//        Logger.logNumberOfResolvedEmergencies()
     }
 }
