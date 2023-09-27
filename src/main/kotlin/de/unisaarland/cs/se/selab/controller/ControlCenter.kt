6e16ac5696b13912a98d10d2027a87c417d5d753
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-28 00:58:47 +0200

    add TODO() to ControlCenter, don't understand why simulate() return boolean

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
@@ -1,6 +1,11 @@
 package de.unisaarland.cs.se.selab.controller
 
-import de.unisaarland.cs.se.selab.controller.phases.*
+import de.unisaarland.cs.se.selab.controller.phases.AssetAllocation
+import de.unisaarland.cs.se.selab.controller.phases.EmergencyDistribution
+import de.unisaarland.cs.se.selab.controller.phases.GatherStatistics
+import de.unisaarland.cs.se.selab.controller.phases.RequestProcessing
+import de.unisaarland.cs.se.selab.controller.phases.Reroute
+import de.unisaarland.cs.se.selab.controller.phases.UpdatePhase
 import de.unisaarland.cs.se.selab.model.Model
 
 /**
@@ -19,10 +24,11 @@ class ControlCenter(val model: Model) {
     /***
      * Runs the simulation for a fixed amount of ticks, then gathers statistics */
     fun simulate(): Boolean {
-        while(model.maxTick == null || model.currentTick < model.maxTick) {
+        while (model.maxTick == null || model.currentTick < model.maxTick) {
             tick()
         }
         gatherStatistics.execute(model)
+        TODO()
     }
 
     /***
@@ -32,7 +38,7 @@ class ControlCenter(val model: Model) {
         assetAllocation.execute(model)
         requestProcessing.execute(model)
         updatePhase.execute(model)
-        if(updatePhase.eventOccured) reroute.execute(model)
+        if (updatePhase.eventOccured) reroute.execute(model)
         model.incrementTick()
     }
 }
