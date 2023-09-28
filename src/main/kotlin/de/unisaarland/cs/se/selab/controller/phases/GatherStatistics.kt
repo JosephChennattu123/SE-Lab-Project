7f5031f05a84547ac4398f5bfa892ad8ce2da9de
Zheyuan <wuzheyuan.per@gmail.com>
2023-09-28 16:49:49 +0200

    add log simulation start,and fix Gather statistics phase

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/GatherStatistics.kt
@@ -15,15 +15,14 @@ class GatherStatistics {
      */
     fun execute(model: Model) {
         Logger.logSimulationEnded()
-        // the number of rerouted assets
-        Logger.logNumberOfReroutedAssets()
-        // the number of received emergencies
-        Logger.logNumberOfRecievedEmergencies(model.emergencies.values.size)
-        // the number of still ongoing emergencies
-        Logger.logNumberOfOngoingEmergencies(model.assignedEmergencies.size)
-        // the number of failed emergencies
-        Logger.logNumberOfFailedEmergencies()
-        // the number of resolved emergencies
-        Logger.logNumberOfResolvedEmergencies()
+        Logger.logStatistics(
+            model.emergencies.filter {
+                it.key < (
+                    model.maxTick
+                        ?: throw IllegalArgumentException("How could the maxTick be null?!")
+                    )
+            }.values.size,
+            model.assignedEmergencies.size
+        )
     }
 }
