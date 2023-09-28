117b50636a2c472dac0ea49ade9b45146a6634ae
Bashar Hamade <baha00002@stud.uni-saarland.de>
2023-09-28 11:38:56 +0100

    fixed linting

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/phases/EmergencyDistribution.kt
@@ -1,60 +1,57 @@
+package de.unisaarland.cs.se.selab.controller.phases
+
 import de.unisaarland.cs.se.selab.model.BaseType
-import de.unisaarland.cs.se.selab.model.Emergency
 import de.unisaarland.cs.se.selab.model.EmergencyType
 import de.unisaarland.cs.se.selab.model.Model
 import de.unisaarland.cs.se.selab.util.Dijkstra
 import de.unisaarland.cs.se.selab.util.Logger
 
 /**
- * Emergency distribution phase
- * In this phase emergencies get distributed to the bases.
+ * The class handles the  distribution of emergencies to the nearest bases
+ * depending on the type of emergency and their locations in the simulation model.
  */
+
 class EmergencyDistribution {
 
+    /**
+     * Executes the emergency distribution process.
+     *
+     * This method retrieves the current emergencies for the current simulation tick, determines the nearest
+     * base for each emergency type, assigns the emergency to the nearest base, adds it to the active emergencies
+     * in the model, and logs the assignment.
+     *
+     * @param model The simulation model containing emergencies, bases, and the graph.
+     */
+    fun execute(model: Model) {
+        // Get emergencies for the current tick.
+        val currentEmergencies = model.getCurrentEmergencies()
 
-    public fun execute(model: Model) {
-        //get emergencies for current tick
-        var currentEmergencies = model.getCurrentEmergencies()
+        // Get the graph from the model.
+        val graph = model.graph
 
-        //get graph from model
-        var graph = model.graph
+        for (e in currentEmergencies) {
+            // Iterate over emergencies and get the nearest base using one of the Dijkstra methods.
+            var nearestBaseId: Int? = 0
+            val loc = e.location
 
-
-        for (e in currentEmergencies)
-        {
-            //iterate over emergencies and get nearest base using one of the dijkstra methods
-            var nearestbaseId : Int = 0
-            var loc = e.location
-
-            //This seems very wrong,but I need to provide a basetype for getnearestbasetoEdge,so I will have multiple if statements for this purpose
-            if(e.type == EmergencyType.CRIME)
-            {
-                nearestbaseId = Dijkstra.getNearestBaseToEdge(graph,loc, BaseType.POLICE_STATION)
+            if (e.type == EmergencyType.CRIME) {
+                nearestBaseId = Dijkstra.getNearestBaseToEdge(graph, loc, BaseType.POLICE_STATION)
             }
-            if(e.type == EmergencyType.FIRE)
-            {
-                nearestbaseId = Dijkstra.getNearestBaseToEdge(graph,loc,BaseType.FIRE_STATION)
+            if (e.type == EmergencyType.FIRE) {
+                nearestBaseId = Dijkstra.getNearestBaseToEdge(graph, loc, BaseType.FIRE_STATION)
             }
-            if(e.type == EmergencyType.ACCIDENT || e.type == EmergencyType.MEDICAL)
-            {
-                nearestbaseId =  Dijkstra.getNearestBaseToEdge(graph,loc,BaseType.HOSPITAL)
+            if (e.type == EmergencyType.ACCIDENT || e.type == EmergencyType.MEDICAL) {
+                nearestBaseId = Dijkstra.getNearestBaseToEdge(graph, loc, BaseType.HOSPITAL)
             }
-            //assign the base Id to the emergency
-            //e.baseId = nearestbaseId
 
-            //assign the emergency Id to the base
-            model.getBaseById(nearestbaseId)?.addEmergency(e.id)
+            // Assign the emergency ID to the base.
+            model.getBaseById(nearestBaseId as Int)?.addEmergency(e.id)
 
+            // Add the emergency to the list of active emergencies in the model.
+            model.addToAssignedEmergencies(nearestBaseId)
 
-            //add emergency to active emergencies in the model
-            model.addToAssignedEmergencies(nearestbaseId)
-
-            Logger.logEmergencyAssigned(e.id,nearestbaseId)
-
+            // Log the assignment.
+            Logger.logEmergencyAssigned(e.id, nearestBaseId)
         }
-
-
-
     }
-
 }
