2ae669f717dcd09c371ef06cce0c350de2314395
Daniel Sherbakov <dash00001@stud.uni-saarland.de>
2023-09-28 18:38:55 +0200

    deleted merge message from code.

--- src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/controller/ControlCenter.kt
@@ -9,54 +9,32 @@ import de.unisaarland.cs.se.selab.controller.phases.UpdatePhase
 import de.unisaarland.cs.se.selab.model.Model
 
 /**
-<<<<<<<<< Temporary merge branch 1
  * ControlCenter handles the different phases of a tick */
-class ControlCenter(
-    var model: Model
-) {
-=========
- * The ControlCenter runs the simulation.
- *
- * @param model contains the data to simulate
- */
+
 class ControlCenter(val model: Model) {
->>>>>>>>> Temporary merge branch 2
     var emergencyDistribution = EmergencyDistribution()
     var assetAllocation = AssetAllocation()
     var requestProcessing = RequestProcessing()
     var updatePhase = UpdatePhase()
     var reroute = Reroute()
     var gatherStatistics = GatherStatistics()
-
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
     /***
      * Runs the simulation for a fixed amount of ticks, then gathers statistics */
     fun simulate(): Boolean {
-<<<<<<<<< Temporary merge branch 1
         while(model.currentTick < model.maxTick ) {
             tick()
         }
         gatherStatistics.execute(model)
-=========
->>>>>>>>> Temporary merge branch 2
-        TODO()
     }
 
     /***
      * Progresses the simulation by 1 tick */
     fun tick() {
-<<<<<<<<< Temporary merge branch 1
         emergencyDistribution.execute(model)
         assetAllocation.execute(model)
         requestProcessing.execute(model)
         updatePhase.execute(model)
         if(updatePhase.eventOccured) reroute.execute(model)
         model.incrementTick()
-=========
         TODO()
->>>>>>>>> Temporary merge branch 2
-    }
 }
