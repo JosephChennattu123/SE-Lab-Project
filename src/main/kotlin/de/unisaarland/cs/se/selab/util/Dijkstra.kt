72fa9eca7f6110759bdf8127d1e858b3397afaa3
Zheyuan <wuzheyuan.per@gmail.com>
2023-10-06 12:26:07 +0200

    fix complex in dijkstra

--- src/main/kotlin/de/unisaarland/cs/se/selab/util/Dijkstra.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/util/Dijkstra.kt
@@ -411,10 +411,12 @@ object Dijkstra {
 //                    ": newDistance $newDistance, oldDistance $oldDistance"
 //            )
 
-            if (newDistance < oldDistance ||
-                !parentMap.contains(vertex.vertexId) ||
-                (newDistance == oldDistance && vertex.vertexId < parentMap.getValue(vertex.vertexId))
-            ) {
+            val isNewShorter = newDistance < oldDistance
+            val isNotInParentMap = !parentMap.contains(vertex.vertexId)
+            val isSameDistanceButSmallerId =
+                newDistance == oldDistance && vertex.vertexId < parentMap.getValue(vertex.vertexId)
+
+            if (isNewShorter || isNotInParentMap || isSameDistanceButSmallerId) {
                 distancesFromSource[newVertexId] = newDistance
                 parentMap[newVertexId] = vertex.vertexId
             }
